1b6a9cc99e96989a51b790126b9a9d789825f856
julydeath <manojkarajada.mk@gmail.com>
2023-03-29 15:16:17 +0530

    chore : http - https

--- src/axios/global.js
+++ src/axios/global.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://todo-server-production-e8c2.up.railway.app';
+axios.defaults.baseURL = 'https://todo-server-production-e8c2.up.railway.app';
